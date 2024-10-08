25d77c1b2468649bd5055e83dc467ffecffc6390
Thomas Basham <Bashamtg@gmail.com>
2024-10-07 18:42:00 -0700

    updated timeout

--- supabaseInstance.js
+++ supabaseInstance.js
@@ -4,10 +4,11 @@ const axios = require("axios");
 
 const instance = axios.create({
   baseURL: process.env.SUPABASE_URL + "/rest/v1",
-  timeout: 2000,
+  timeout: 3000,
   headers: {
     apikey: process.env.SUPABASE_KEY,
     Authorization: "Bearer " + process.env.SUPABASE_KEY,
+    Prefer: "return=representation", // This ensures the inserted data is returned
   },
 });
 
