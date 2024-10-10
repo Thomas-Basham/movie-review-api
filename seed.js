0f9f87f346198865a9cdffe8f45e4896aab95fb3
Thomas Basham <Bashamtg@gmail.com>
2024-10-10 11:33:57 -0700

    updated seed data function

--- seed.js
+++ seed.js
@@ -5,12 +5,13 @@ const supabase = require("./supabaseInstance");
 // Function to search movies by genre and seed data into the database
 async function seedMovies() {
   try {
+    let page = 1;
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=4&sort_by=popularity.desc`,
+      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&primary_release_year=2024`,
       {
         headers: {
           accept: "application/json",
-          Authorization: `Bearer ${process.env.IMDB_API_KEY}`,
+          Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
         },
       }
     );
@@ -20,27 +21,19 @@ async function seedMovies() {
     }
     console.log(data);
 
-    const movieDataList = [];
-
-    // Loop through the search results and fetch full details for each movie
     for (const movie of data.results) {
-      // const { data: movieDetails } = await axios.get(
-      //   `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`
-      // );
       console.log(movie);
       const movieData = {
         title: movie.title,
         release_year: new Date(movie.release_date).getFullYear(),
-        // genre: movieDetails.Genre,
-        // director: movieDetails.Director,
-        // actors: movieDetails.Actors,
-        // plot: movieDetails.Plot,
-        // poster: movieDetails.Poster,
-        // imdb_rating: movieDetails.imdbRating,
+        overview: movie.overview,
+        TMDBid: movie.id,
+        poster_path: movie.poster_path,
+        backdrop_path: movie.backdrop_path,
       };
       console.log(movieData);
-      const { data, error } = supabase.post("movie", movieData);
 
+      const { data, error } = supabase.post("movie", movieData);
       if (error) {
         console.log({ error });
       }
